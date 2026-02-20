7872da1158d4f215aa29d3a814d952f8f721fa71
priyajoji <priyajoji212@gmail.com>
2026-02-20 09:36:41 +0530

    UC1:Add welcome message module

--- src/UseCase1PalindromeCheckerApp.java
+++ src/UseCase1PalindromeCheckerApp.java
@@ -1,10 +1,7 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class UseCase1PalindromeCheckerApp {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-
+        System.out.println("Welcome to the Palindrome Checker Management System\nVersion : 1.0\nSystem initialized successfully");
         }
     }
-}
